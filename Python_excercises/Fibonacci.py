3fef71cb0f96a89d244ad021d4fbf9d1b1089be9
Edison0019 <edison_0019@hotmail.com>
2020-03-30 11:52:08 -0400

    added new program

---
+++ Python_excercises/Fibonacci.py
@@ -0,0 +1,22 @@
+def fibonacci(n):
+    s = str(type(n))
+    s = 'int' in s
+    f = str(type(n))
+    f = 'float' in f
+    if s == False:
+        return print('the paramenter must me an integer not text nor float')
+    elif n < 1:
+        n = 1
+    else:
+        l = [0,1]
+        i = 0
+        o = 0
+        while i < n - 1:
+            o = (l[-1] + l[-2])
+            l.append(o)
+            i += 1
+        return l
+
+output = fibonacci(10.8)
+print('the list of values is',output)
+print('the last value is',output[-1])
